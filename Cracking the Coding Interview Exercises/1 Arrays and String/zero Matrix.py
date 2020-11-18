e7064af2b883b84193e8ec7c501f54c45edefe70
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-11-18 08:57:39 +0530

    Update zero Matrix.py

--- Cracking the Coding Interview Exercises/1 Arrays and String/zero Matrix.py
+++ Cracking the Coding Interview Exercises/1 Arrays and String/zero Matrix.py
@@ -1,6 +1,6 @@
 '''1.8 Zero Matrix: Write an algorithm such that if an element in an
 MxN matrix is 0, its entire row and
-column are set to 0.'''
+column are set to 0. '''
 
 def zeromateix(arr):
     r = []  # for rows
