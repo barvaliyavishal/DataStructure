3b3c2e6bb13bb45415d29f851e76c221cbcece30
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-11-18 07:13:47 +0530

    Update 1.7   Rotate Matrix.py
    Improve algorithm to return new array of ratated matrix instead of printing it.

--- Cracking the Coding Interview Exercises/1 Arrays and String/1.7   Rotate Matrix.py
+++ Cracking the Coding Interview Exercises/1 Arrays and String/1.7   Rotate Matrix.py
@@ -5,13 +5,23 @@ Hints: #51, # 100'''
 
 
 def rotateMatrix(arr):
-    temp=0;
-    for i in range(len(arr[0])):
-        for j in range(len(arr[1])-1,-1,-1):
-            print(arr[j][i],end=" ")
-        print();
 
-rotateMatrix([[1,  2,  3,  4],
-              [5,  6,  7,  8],
-              [9,  10, 11, 12],
-              [13, 14, 15, 16]])
+    arr2=[];
+    for i in range(len(arr[0])):
+        temp = [];
+        for j in range(len(arr[1])-1,-1,-1):
+            temp.append(arr[j][i]);
+        arr2.append(temp)
+    return arr2;
+
+a = rotateMatrix([[1,  2,  3,  4],
+                  [5,  6,  7,  8],
+                  [9,  10, 11, 12],
+                  [13, 14, 15, 16]])
+
+for i in range(len(a[0])):
+    for j in range(len(a[1])):
+        print(a[i][j],end=" ");
+    print()
+
+
