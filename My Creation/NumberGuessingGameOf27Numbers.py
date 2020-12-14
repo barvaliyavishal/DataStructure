ae8c6143dd43e596737a137a4bf8285d56fba0c1
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-12-14 21:28:00 +0530

    Create NumberGuessingGameOf27Numbers.py
    this is also a number gussing game which is totally implemented from my real life ideas  and experience

---
+++ My Creation/NumberGuessingGameOf27Numbers.py
@@ -0,0 +1,92 @@
+arr = []
+for i in range(27):
+    arr.append(i+1)
+print()
+print('First Of All Welcome to this amazing Game Created By "Vishal Barvaliya"')
+print()
+print("Assume or guess any Number from given Table and follow instruction Given Below")
+print()
+for i in range(3):
+    print("|------+------+------|")
+    print("|  1   |   2  |  3   |")
+    print("|------+------+------|")
+    index = 0
+    first = []
+    second = []
+    third = []
+    for j in range(9):
+        print("|", end="  ")
+        print(arr[index], end="")
+        first.append((arr[index]))
+        if arr[index] < 10:
+            print("   |  ", end="")
+        else:
+            print("  |  ", end="")
+        index += 1
+        print(arr[index], end="")
+        second.append((arr[index]))
+        if arr[index] < 10:
+            print("   |  ", end="")
+        else:
+            print("  |  ", end="")
+        index += 1
+        print(arr[index], end="")
+        third.append((arr[index]))
+        if arr[index] < 10:
+            print("   |  ", end="")
+        else:
+            print("  |  ", end="")
+        index += 1
+
+        print()
+    print("|------+------+------|")
+
+
+    while(True):
+        print()
+
+        k = int(input("Enter the column number (ex : 1, 2 or 3) in which your number is available  :  "))
+        if k == 1:
+            ind = 0
+            for l in second:
+                arr[ind] = l
+                ind += 1
+            for l in first:
+                arr[ind] = l
+                ind += 1
+            for l in third:
+                arr[ind] = l
+                ind += 1
+            break
+        elif k == 3:
+            ind = 0
+            for l in second:
+                arr[ind] = l
+                ind += 1
+            for l in third:
+                arr[ind] = l
+                ind += 1
+            for l in first:
+                arr[ind] = l
+                ind += 1
+            break
+        if k == 2:
+            ind = 0
+            for l in first:
+                arr[ind] = l
+                ind += 1
+            for l in second:
+                arr[ind] = l
+                ind += 1
+            for l in third:
+                arr[ind] = l
+                ind += 1
+            break
+        else:
+            print("enter Valid input please!!!");
+print()
+print()
+print("***********************************************")
+print()
+print()
+print("Congratulations!!! I Found Your Number Which is  :  ", arr[13])
