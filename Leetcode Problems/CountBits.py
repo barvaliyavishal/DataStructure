05d35367c90897a451c19d92c8368846415dcde9
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-12-22 11:42:37 +0530

    Update CountBits.py

--- Leetcode Problems/CountBits.py
+++ Leetcode Problems/CountBits.py
@@ -3,6 +3,16 @@ Given a non negative integer number num.
  For every numbers i in the range 0 ≤ i ≤ num c
  alculate the number of 1's in their binary representation
  and return them as an array.
+
+Example 1:
+    Input: 2
+    Output: [0,1,1]
+
+Example 2:
+    Input: 5
+    Output: [0,1,1,2,1,2]
+
+
 '''
 
 def countBits(num: int):
