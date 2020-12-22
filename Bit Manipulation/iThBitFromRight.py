067164f3b5d921e3c2a1f71e26883ece49482445
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-12-22 11:46:35 +0530

    Update iThBitFromRight.py

--- Bit Manipulation/iThBitFromRight.py
+++ Bit Manipulation/iThBitFromRight.py
@@ -1,7 +1,6 @@
 ''' Check the iTh bit is 1 or 0 from right '''
 
 def iThBitFromRight(n, k):
-    for i in range(k-1):
-        n = n >> 1
-    return n & 1
-print(iThBitFromRight(11,2))
+    return (n & (1 << k-1))!=0
+
+print(iThBitFromRight(7,3))
