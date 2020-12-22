91f27850f4b2e705ff02de68396881ee687b1f31
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2020-12-22 16:28:14 +0530

    Update number of 1 bits.py

--- Bit Manipulation/number of 1 bits.py
+++ Bit Manipulation/number of 1 bits.py
@@ -5,12 +5,9 @@ Write a function that takes an unsigned integer and
 returns the number of '1' bits it has
 '''
 def numOfOnes(n: int) -> int:
-    if n == 0:
-        return 0
-    res = 1
-    temp = n & n-1
-    while temp > 0:
+    res = 0
+    while n:
         res += 1
-        temp = temp & temp-1
+        n = n & n - 1
     return res
 print(numOfOnes(7))
