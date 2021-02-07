5c6fe63c646160d54aec5dccad6033f547c63d47
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-02-07 09:30:27 +0530

    Create IsPalindrome.py
    check weather linked list is palindrome or not using recursion which take O(n) time and space complexity

---
+++ Cracking the Coding Interview Exercises/Linked Lists/SinglyLinkedList/IsPalindrome.py
@@ -0,0 +1,35 @@
+from LinkedList import LinkedList
+
+def is_palindrome(head):
+    t = head
+    flag = True
+
+    def sub(temp):
+        if temp is None:
+            return
+        sub(temp.next)
+        nonlocal flag
+        nonlocal head
+        if flag and temp.data != head.data:
+            flag = False
+        head = head.next
+    sub(t)
+    if flag:
+        return "Palindrome"
+    else:
+        return "Not Palindrome"
+
+
+obj1 = LinkedList()
+
+obj1.add("e")
+obj1.add("e")
+# obj1.add("b")
+# obj1.add("c")
+# obj1.add("b")
+# obj1.add("a")
+# obj1.add("e")
+
+
+res = is_palindrome(obj1.head)
+print(res)
