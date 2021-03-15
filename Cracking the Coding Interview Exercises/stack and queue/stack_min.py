612a103313d4b66a0c39fbd70af7b6ab73a35bb5
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-03-15 18:03:59 +0530

    Update stack_min.py

--- Cracking the Coding Interview Exercises/stack and queue/stack_min.py
+++ Cracking the Coding Interview Exercises/stack and queue/stack_min.py
@@ -1,10 +1,41 @@
-class Stack:
+class MinStack:
+
     def __init__(self):
-        self.arr = []
-        self.top = -1
+        self.stack = []
         self.min = None
 
-    def push(self, val):
-        self.top+=1
-        if self.top < len(self.arr):
-            self.arr[self.top] = 0
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if self.min == None:
+            self.min = x
+        else:
+            self.min = min(x, self.min)
+
+    def pop(self) -> None:
+        element = self.stack.pop()
+        if not self.stack:
+            self.min = None
+        if element == self.min:
+            self.min = min(self.stack)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
+
+obj = MinStack()
+obj.push(10)
+obj.push(4)
+obj.push(9)
+obj.push(9)
+obj.push(1)
+obj.push(1)
+print(obj.top())
+
+obj.pop()
+obj.pop()
+obj.pop()
+obj.pop()
+obj.pop()
+print(obj.getMin())
