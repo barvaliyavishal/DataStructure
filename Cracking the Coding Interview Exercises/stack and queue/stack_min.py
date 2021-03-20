ebc8fc24f954b2fb51593fc6e2697e84c55c9eb4
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-03-20 15:30:56 +0530

    Update stack_min.py

--- Cracking the Coding Interview Exercises/stack and queue/stack_min.py
+++ Cracking the Coding Interview Exercises/stack and queue/stack_min.py
@@ -2,40 +2,69 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min = None
+        self.min = []
+        self.topmin = -1
+        self.top = -1
+
+    def minval(self,val):
+        if self.topmin + 1 == len(self.min):
+            if self.topmin >= 0 and self.min[self.topmin] >= val:
+                self.min.append(val)
+                self.topmin += 1
+            elif self.topmin == -1:
+                self.topmin += 1
+                self.min.append(val)
+        else:
+            if self.topmin == -1:
+                self.topmin += 1
+                self.min[self.topmin] = val
+            else:
+                if self.min[self.topmin] >= val:
+                    self.topmin += 1
+                    self.stack[self.topmin] = val
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        if self.min == None:
-            self.min = x
+        if self.top+1 == len(self.stack):
+            self.stack.append(x)
+            self.top += 1
         else:
-            self.min = min(x, self.min)
+            self.top += 1
+            self.stack[self.top] = x
+        self.minval(x)
 
     def pop(self) -> None:
-        element = self.stack.pop()
-        if not self.stack:
-            self.min = None
-        if element == self.min:
-            self.min = min(self.stack)
+        if self.top >= 0:
+            if self.stack[self.top] == self.min[self.topmin]:
+                self.top -=1
+                self.topmin -= 1
+            else:
+                self.top -= 1
 
-    def top(self) -> int:
-        return self.stack[-1]
+    def topp(self):
+        if self.top != -1:
+            return self.stack[self.top]
 
     def getMin(self) -> int:
-        return self.min
+        if self.topmin != -1:
+            return self.min[self.topmin]
+
 
 obj = MinStack()
-obj.push(10)
-obj.push(4)
-obj.push(9)
-obj.push(9)
-obj.push(1)
-obj.push(1)
-print(obj.top())
-
-obj.pop()
-obj.pop()
-obj.pop()
-obj.pop()
-obj.pop()
-print(obj.getMin())
+flag = True
+while flag:
+    inp = int(input("1 for push "
+                    " 2 for pop "
+                    "3 for get min "
+                    "4 for top 5 for exit " ))
+    if inp == 1:
+        obj.push(int(input("enter value to push")))
+    elif inp == 2:
+        print(obj.pop())
+    elif inp == 3:
+        print(obj.getMin())
+    elif inp == 4:
+        print(obj.topp())
+    else:
+        flag = False
+    print(obj.stack)
+    print(obj.min)
