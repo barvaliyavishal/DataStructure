7f809da89ee73ed4f722b352b00ba6efa4b71159
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-04-07 08:44:04 +0530

    Create 901. Online Stock Span.py

---
+++ Leetcode Problems/901. Online Stock Span.py
@@ -0,0 +1,19 @@
+class StockSpanner(object):
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
+        weight = 1
+        while self.stack and self.stack[-1][0] <= price:
+            weight += self.stack.pop()[1]
+        self.stack.append((price, weight))
+        return weight
+
+obj = StockSpanner()
+print(obj.next(100))
+print(obj.next(80))
+print(obj.next(60))
+print(obj.next(70))
+print(obj.next(60))
+print(obj.next(75))
+print(obj.next(85))
